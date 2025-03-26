d804f8e314ac8e113551db48719f6e2baa9e5d96
seb-alliot <sebastienalliot04@gmail.com>
2025-03-26 18:33:18 +0100

    correctif settings

--- ECORIDE/settings.py
+++ ECORIDE/settings.py
@@ -120,7 +120,7 @@ MEDIA_URL = '/main/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'main', 'profils_picture')
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static'),
