26733fa6ec81d3013e14f5e4ce7cf219e70dae90
seb-alliot <sebastienalliot04@gmail.com>
2025-04-09 23:05:45 +0200

    test root en moins pour static

--- ECORIDE/settings.py
+++ ECORIDE/settings.py
@@ -145,12 +145,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'main', 'profils_picture')
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'ECORIDE', 'static'),
-]
-
-
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
