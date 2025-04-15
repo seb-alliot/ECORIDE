fb42ce1cdb040221e8c65c85ef615f79735d16fe
seb-alliot <sebastienalliot04@gmail.com>
2025-04-16 00:30:11 +0200

    je verifi un changement

--- ECORIDE/settings.py
+++ ECORIDE/settings.py
@@ -137,7 +137,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "main","media")
 
-STATIC_URL = "/staticfiles/"
+STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_DIRS = [
