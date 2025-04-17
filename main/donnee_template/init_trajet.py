a790b30584bf52ff7a337c64623082193e66a759
seb-alliot <sebastienalliot04@gmail.com>
2025-04-17 23:26:54 +0200

    soucis de donnée sur une page si null

--- main/donnee_template/init_trajet.py
+++ main/donnee_template/init_trajet.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from ..models import TrajetProposer, Voiture
 from collections import defaultdict
 
-
 def InfoTrajet(request):
     user = request.user
     type_moteur = Voiture.objects.filter(type_moteur=user)
@@ -10,13 +9,18 @@ def InfoTrajet(request):
     chauffeur = TrajetProposer.objects.filter(chauffeur=user).first()
     trajets = TrajetProposer.objects.filter(chauffeur=user)
     trajet_chercher = TrajetProposer.objects.filter(chauffeur=user, etat__in=["Terminé", "Annulé", "En cours", "Disponible"])
+
     trajet_voulu = defaultdict(list)
+    # Initialiser les listes vides dès le départ
+    trajet1, trajet2, trajet3, trajet4 = [], [], [], []
+
     for trajet in trajet_chercher:
         trajet_voulu[trajet.etat].append(trajet)
-        trajet1 = trajet_voulu["Terminé"]
-        trajet2 = trajet_voulu["Annulé"]
-        trajet3 = trajet_voulu["En cours"]
-        trajet4 = trajet_voulu["Disponible"]
+
+    trajet1 = trajet_voulu["Terminé"]
+    trajet2 = trajet_voulu["Annulé"]
+    trajet3 = trajet_voulu["En cours"]
+    trajet4 = trajet_voulu["Disponible"]
 
     return {
         'chauffeur': chauffeur,
