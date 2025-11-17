37b0fd84e96e4fda3cbbf7c11d5a2b1cf3cefc9f
seb-alliot <sebastienalliot04@gmail.com>
2025-11-17 14:13:48 +0100

    suppression des msg dans le console log en prod...

--- staticfiles/js/moderation/selection_email.js
+++ staticfiles/js/moderation/selection_email.js
@@ -2,12 +2,12 @@ document.addEventListener("DOMContentLoaded", function () {
     const select = document.getElementById("email_type");
     const form = document.getElementById("email_filter_form");
 
-    if (!select || !form) return; 
+    if (!select || !form) return;
 
     select.addEventListener("change", function () {
         form.submit();
     });
     if (select.value === "" || select.value === null) {
-        console.log("Aucun changement sélectionné, formulaire non soumis");
+        return;
     }
 });
